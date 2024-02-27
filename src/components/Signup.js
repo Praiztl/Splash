10e82c642a2750838d19757dd599a78dd408e3da
nightburnn <nightburn2003@gmail.com>
2024-02-27 21:56:51 +0100

    fixed hexagon issue

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,17 +1,18 @@
 import React from 'react'
 import './Signup.css'
 import hexagon from '../assets/hexagon.gif';
-
+import hexagonPhone  from '../assets/hexagonPhone.gif'
 const Signup = () => {
   return (
     <div className="container">
         <div className='hexagon'>
-        <img src={hexagon} alt=''/>
+        <img className='desktop' src={hexagon} alt=''/>
+        <img  className="phone" src={hexagonPhone} alt=''/>
         </div>
 
         <div className='sign'>
         <h1 className="step-title">Welcome to Shyn Tech</h1>
-        <p className="text">With Shine Coin Technology, you can securely deposit, earn and trad NFTs or Cryptocurrency</p>
+        <p className="text">With Shine Coin Technology, you can securely deposit, earn and trade NFTs or Cryptocurrency</p>
         <button className="button" >Sign Up</button>
         <p className="text">Already have an account? <a href="#" className="link">Sign in</a></p>
         </div>
